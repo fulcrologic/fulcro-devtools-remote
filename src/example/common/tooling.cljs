1902ec485cdb477414650fd2c472625f063645ea
Tony Kay <tony.kay@gmail.com>
2024-12-02 19:04:58 -0500

    Working on sample tool

--- src/example/common/tooling.cljs
+++ src/example/common/tooling.cljs
@@ -3,6 +3,6 @@
     [com.fulcrologic.fulcro.mutations :as m]))
 
 (m/defmutation restart [_]
-  (ok-action [env] (js/console.log "Mutation returned result" (keys env)))
+  (ok-action [env] (js/console.log "Restart returned result" (keys env)))
   (error-action [env] (js/console.log "Restart failed (timeout?)"))
   (remote [_] true))
--- src/example/devtool/ui.cljs
+++ src/example/devtool/ui.cljs
@@ -1,5 +1,6 @@
 (ns devtool.ui
   (:require
+    [cljs.pprint :refer [pprint]]
     [clojure.edn :as edn]
     [com.fulcrologic.devtools.chrome.devtool :refer [active-target-descriptors]]
     [com.fulcrologic.devtools.message-keys :as mk]
@@ -9,6 +10,7 @@
     [com.fulcrologic.fulcro.dom :as dom]
     [com.fulcrologic.fulcro.dom.events :as evt]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+    [common.tooling :as tooling]
     [taoensso.timbre :as log]))
 
 (defmutation counter-added [{:tool/keys [id]}]
@@ -35,6 +37,9 @@
                    :ui/counters    0}}
   (dom/div
     (dom/h3 (str label))
+    (dom/button
+      {:onClick (fn [] (comp/transact! this [(tooling/restart {mk/target-id id})]))}
+      "RESET")
     (dom/div
       (str "Counters: " counters))
     (dom/div
@@ -44,22 +49,27 @@
 
 (m/defmutation set-active-tool [{:tool/keys [id label] :as tool}]
   (action [{:keys [state]}]
-    (if (get-in @state [:tool/id id])
-      (swap! state assoc :ui/active-tool [:tool/id id])
-      (swap! state merge/merge-component Tool tool :replace [:ui/active-tool]))))
+    (when id
+      (if (get-in @state [:tool/id id :tool/id])
+        (swap! state assoc :ui/active-tool [:tool/id id])
+        (swap! state merge/merge-component Tool tool :replace [:ui/active-tool])))))
 
-(defsc Root [this {:ui/keys [active-tool]}]
+(defsc Root [this {:ui/keys [active-tool] :as props}]
   {:query         [{:ui/active-tool (comp/get-query Tool)}]
    :initial-state {}}
   (let [tds @active-target-descriptors]
     (dom/div nil
+      (dom/pre
+        (with-out-str
+          (pprint (app/current-state this))))
       (dom/h4 nil "Tool UI")
       (dom/select {:value    (pr-str (:tool/id active-tool))
                    :onChange (fn [evt]
-                               (let [id    (edn/read-string (evt/target-value evt))
+                               (let [id    (log/spy :info (edn/read-string (evt/target-value evt)))
                                      label (get @active-target-descriptors id)]
                                  (comp/transact! this [(set-active-tool {:tool/id    id
                                                                          :tool/label label})])))}
+        (dom/option {:value "nil"} "Select an app")
         (mapv
           (fn [tid] (dom/option {:key   (str tid)
                                  :value (pr-str tid)}
